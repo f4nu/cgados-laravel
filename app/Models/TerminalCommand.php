6ae81379cf4ac4ca439e0fe1bf3b4f5bff8393b5
Mattia Fanu Ceruti <mattia@abiby.it>
2024-04-11 22:32:06 +0200

    Updated intro command

--- app/Models/TerminalCommand.php
+++ app/Models/TerminalCommand.php
@@ -21,28 +21,49 @@ class TerminalCommand extends Model
     }
 
     public function intro(string $string): string {
-        $totalDiskCheckPosition = 547556790632448;
-        $endDate = "2024-05-22";
-        $diff = (new Carbon($endDate))->diffInSeconds(new Carbon("2024-04-06"));
-        $now = (new Carbon($endDate))->diffInSeconds();
-        // currentDisk : totalDisk = diff - now : diff
-        $currentDiskCheckPosition = (int)($totalDiskCheckPosition * ($diff - $now) / $diff);
-        $percent = (int)($currentDiskCheckPosition / $totalDiskCheckPosition * 10000) / 100;
+            $startDate = '2024-04-06';
+            $endDate = '2025-05-22';
+            $pinDate = '2024-05-22';
 
-        $modifier = 1.7;
-        $barMax = 100 / $modifier;
-        $emptyCharacter = '░';
-        $fullCharacter = '█';
+            $startDateUnix = (new Carbon($startDate))->unix();
+            $endDateUnix = (new Carbon($endDate))->unix();
+            $nowUnix = (new Carbon())->unix();
+            $pintDateUnix = (new Carbon($pinDate))->unix();
 
-        $bar = '[';
-        for ($i = 0; $i < $barMax; $i++) {
-            if ($i <= $percent / $modifier)
-                $bar .= $fullCharacter;
-            else
-                $bar .= $emptyCharacter;
-        }
-        $bar .= ']';
+            $endRelative = $endDateUnix - $startDateUnix;
+            $nowRelative = $nowUnix - $startDateUnix;
+            $pinRelative = $pintDateUnix - $startDateUnix;
 
-        return sprintf($string, $currentDiskCheckPosition, $totalDiskCheckPosition, $percent, $bar);
+            $nowPercent = (int)(($nowRelative * 100 / $endRelative) * 1000) / 1000;
+            $pinPercent = (int)(($pinRelative * 100 / $endRelative) * 1000) / 1000;
+
+            $totalDiskCheckPosition = 547556790632448;
+            $currentDiskCheckPosition = (int)($totalDiskCheckPosition / $nowPercent);
+
+            $modifier = 1.5;
+            $barMax = 100 / $modifier;
+            $emptyCharacter = '░';
+            $fullCharacter = '█';
+
+            $bar = '[';
+            for ($i = 1; $i <= $barMax; $i++) {
+                if ($i <= $nowPercent / $modifier)
+                    $bar .= $fullCharacter;
+                else
+                    $bar .= $emptyCharacter;
+            }
+            $bar .= ']';
+
+            $bottomBar = ' ';
+            $pinPercentPosition = (int)($pinPercent * $barMax / 100);
+            $pinBottomBarActivationThreshold = (int)($pinPercentPosition / $modifier);
+            for ($i = 1; $i <= $barMax; $i++) {
+                if ($i < $pinBottomBarActivationThreshold)
+                    $bottomBar .= ' ';
+                else if ($i == $pinBottomBarActivationThreshold)
+                    $bottomBar .= '^';
+            }
+
+        return sprintf($string, $currentDiskCheckPosition, $totalDiskCheckPosition, $nowPercent, $bar, $bottomBar);
     }
 }
